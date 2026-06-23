28ba7e6b9a829f14fa5afb8ce8339648afdd83b0
ankur <aankur00000@gmail.com>
2026-06-23 23:57:47 +0530

    update

--- client/src/services/Linkurl.js
+++ client/src/services/Linkurl.js
@@ -1,3 +1,3 @@
- const baseUrl = 'https://ai-mock-interview-9k9r.onrender.com';
-//  const baseUrl = "http://localhost:6789/api"
+//  const baseUrl = 'https://ai-mock-interview-9k9r.onrender.com';
+  const baseUrl = "http://localhost:6789/api"
 export default baseUrl;
